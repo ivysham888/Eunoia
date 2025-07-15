dbd7afa8cf32850485cb536e81f7967ef6facc29
Ivy Sham <ivysham888@gmail.com>
2025-07-15 23:19:08 +0800

    created all pages / views

---
+++ Eunoia/AboutPage.swift
@@ -0,0 +1,18 @@
+//
+//  AboutPage.swift
+//  Eunoia
+//
+//  Created by Ivy Sham on 15/7/2025.
+//
+
+import SwiftUI
+
+struct AboutPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutPage()
+}
--- Eunoia/EunoiaApp.swift
+++ Eunoia/EunoiaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct EunoiaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomePage()
         }
     }
 }
