4f4d5e9badc096aa2edbaabacd3fbeb916efaf5d
Ivy Sham <ivysham888@gmail.com>
2025-07-15 22:13:18 +0800

    Initial Commit

---
+++ Eunoia/EunoiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EunoiaApp.swift
+//  Eunoia
+//
+//  Created by Ivy Sham on 15/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct EunoiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
