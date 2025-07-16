ec62ddc5ff9e3a263eb020e7ff0adbb4ba61c7e8
Anushka <anushkak1024@gmail.com>
2025-07-16 13:18:54 -0400

    Without errors

---
+++ Eunoia/QuickTipsView.swift
@@ -0,0 +1,18 @@
+//
+//  QuickTipsView.swift
+//  Eunoia
+//
+//  Created by Anushka Karthikeyan on 7/16/25.
+//
+
+import SwiftUI
+
+struct QuickTipsView: View {
+    var body: some View {
+        ZStack
+    }
+}
+
+#Preview {
+    QuickTipsView()
+}
