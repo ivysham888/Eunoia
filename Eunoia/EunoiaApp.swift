4f93644840df60c7c429c83309e52cd0dc9250c3
Nora Martin <norakathleenmartin@gmail.com>
2025-07-16 13:02:42 -0400

    In the process of adding a list feature to the physical page. Also added blurbs about us for the about us page.

--- Eunoia/EunoiaApp.swift
+++ Eunoia/EunoiaApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct EunoiaApp: App {
     var body: some Scene {
         WindowGroup {
             HomePage()
+                .modelContainer(for: Workout.self)
         }
     }
 }
--- Eunoia/PhysicalPage.swift
+++ Eunoia/PhysicalPage.swift
@@ -6,17 +6,105 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct PhysicalPage: View {
+    
+    @State private var showNewTask = false
+    @Query var workouts: [Workout]
+    
     var body: some View {
         ZStack {
             Rectangle()
                 .fill(Color.tanbackground)
                 .ignoresSafeArea()
-        }
+            
+            VStack {
+            Text("Physical Health")
+                    .font(.title)
+                    .fontWeight(.black)
+                    .foregroundColor(Color("brown1"))
+                    .padding(.top)
+                        
+                        
+                Text("What is your goal for today?")
+                    .font(.title2)
+                    .fontWeight(.thin)
+                    .foregroundColor(Color("green1"))
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 12.0)
+                
+                HStack {
+                    NavigationLink(destination: AboutPage()) {
+                        Text("Cardio")
+                            .frame(width: 90, height: 25)
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.white)
+                            .font(.system(size: 20))
+                            .padding()
+                            .background(Color.brown2.clipShape(.rect(cornerRadius: 15)))
+                    }//nav link
+                    
+                    NavigationLink(destination: AboutPage()) {
+                        Text("Weights")
+                            .frame(width: 90, height: 25)
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.white)
+                            .font(.system(size: 20))
+                            .padding()
+                            .background(Color.brown2
+                                .clipShape(.rect(cornerRadius: 15)))
+                    }//nav link
+                    
+                    NavigationLink(destination: AboutPage()) {
+                        Text("Yoga")
+                            .frame(width: 90, height: 25)
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.white)
+                            .font(.system(size: 20))
+                            .padding()
+                            .background(Color.brown2
+                                .clipShape(.rect(cornerRadius: 15)))
+                    }//nav link
+                }
+                
+                HStack{
+                    Text("Track your exercise")
+                        .font(.title2)
+                        .fontWeight(.black)
+                        .foregroundColor(Color("brown1"))
+                    Spacer()
+                    
+                    Button {
+                          showNewTask = true
+                    } label: {
+                        Text("+")
+                            .font(.title)
+                            .fontWeight(.black)
+                            .foregroundColor(Color("brown1"))
+                    }
+                    
+                }//hstack
+                .padding()
+                Spacer()
+                
+                List {
+                    ForEach (workouts) {workouts in
+                        Text(workouts.title)
+                    }
+                }//list
+                .background(Color.tanbackground)
+                .scrollContentBackground(.hidden)
+                .listStyle(.plain)
+                
+            }//vstack
+            if showNewTask{
+                NewAddWorkoutView(workout: Workout(title: "", isImportant: false))
+            }
+        }//zstack
 
-    }
-}
+    }//body
+}//struct
 
 #Preview {
     PhysicalPage()
